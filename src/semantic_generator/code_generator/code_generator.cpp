b5dd83dee2d6ee916b331ad0a345f7b6231bfa47
ivoff <pyeroevo@gmail.com>
2022-03-25 18:06:02 -0400

    formatação do print da quadrupla

--- src/semantic_generator/code_generator/code_generator.cpp
+++ src/semantic_generator/code_generator/code_generator.cpp
@@ -1,36 +1,42 @@
 #include "code_generator.hpp"
 
 int CodeGenerator::add_code(std::string op, std::string argument1, std::string argument2, std::string result) {    
-    m_instructions.push_back({op, argument1, argument2, result});    
+    m_instructions.push_back({op, argument1, argument2, result});
+    update_op_max_length(op, argument1, argument2, result);
     return m_cur_line++;
 }
 
 int CodeGenerator::add_code(std::string op, double argument1, double argument2, std::string result) {
     m_instructions.push_back({op, std::to_string(argument1), std::to_string(argument2), result});
+    update_op_max_length(op, std::to_string(argument1), std::to_string(argument2), result);
     return m_cur_line++;
 };
 
 int CodeGenerator::add_code(std::string op, int argument1, int argument2, std::string result) {
     m_instructions.push_back({op, std::to_string(argument1), std::to_string(argument2), result});
+    update_op_max_length(op, std::to_string(argument1), std::to_string(argument2), result);
     return m_cur_line++;
 };
 
 int CodeGenerator::add_code(std::string op, double argument1, int argument2, std::string result) {
     m_instructions.push_back({op, std::to_string(argument1), std::to_string(argument2), result});
+    update_op_max_length(op, std::to_string(argument1), std::to_string(argument2), result);
     return m_cur_line++;
 };
 
 int CodeGenerator::add_code(std::string op, int argument1, double argument2, std::string result) {
     m_instructions.push_back({op, std::to_string(argument1), std::to_string(argument2), result});
+    update_op_max_length(op, std::to_string(argument1), std::to_string(argument2), result);
     return m_cur_line++;
 };
 
 
 void CodeGenerator::print() {
     int count = 0;
+    std::string print_format = "%03d| %"+std::to_string(m_op_max_length[0])+"s| %"+std::to_string(m_op_max_length[1])+"s| %"+std::to_string(m_op_max_length[2])+"s| %"+std::to_string(m_op_max_length[3])+"s\n";
     for(auto itr = m_instructions.begin(); itr != m_instructions.end(); itr++)
     {
-        printf("%03d; %s; %s; %s; %s\n", count++, itr->at(0).c_str(), itr->at(1).c_str(), itr->at(2).c_str(), itr->at(3).c_str());
+        printf(print_format.c_str(), count++, itr->at(0).c_str(), itr->at(1).c_str(), itr->at(2).c_str(), itr->at(3).c_str());
     }    
 }
 
@@ -48,4 +54,12 @@ void CodeGenerator::edit(int line, int pos, std::string val)
     {
         throw ex;
     }    
+}
+
+void CodeGenerator::update_op_max_length(std::string op0, std::string op1, std::string op2,std::string op3)
+{
+    m_op_max_length[0] = std::max(m_op_max_length[0], (int)op0.size());
+    m_op_max_length[1] = std::max(m_op_max_length[1], (int)op1.size());
+    m_op_max_length[2] = std::max(m_op_max_length[2], (int)op2.size());
+    m_op_max_length[3] = std::max(m_op_max_length[3], (int)op3.size());        
 }
--- src/semantic_generator/code_generator/code_generator.hpp
+++ src/semantic_generator/code_generator/code_generator.hpp
@@ -10,6 +10,7 @@ struct CodeGenerator {
     std::vector<std::array<std::string, 4>> m_instructions;
     int m_temp_var_acc {1};
     int m_cur_line {0};
+    int m_op_max_length[4] {0, 0, 0, 0};
 
     CodeGenerator(){};
     int add_code(std::string op, std::string argument1, std::string argument2, std::string result);
@@ -17,7 +18,8 @@ struct CodeGenerator {
     int add_code(std::string op, int argument1, int argument2, std::string result);
     int add_code(std::string op, double argument1, int argument2, std::string result);
     int add_code(std::string op, int argument1, double argument2, std::string result);
-    std::string gen_temp();    
+    void update_op_max_length(std::string op0, std::string op1, std::string op2, std::string op3);
+    std::string gen_temp();
     void print();
     void edit(int line, int pos, std::string val);
 };
